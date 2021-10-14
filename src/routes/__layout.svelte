c9638ece313fb775f1dbb4b513e7b46109c24fb1
abushak <ihor.kashuba@gmail.com>
2021-10-14 10:13:42 +0300

    feat(working-ws-comfig): :rocket: working-ws-comfig

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,19 @@
 <script>
+	
 	import '../app.css';
-</script>
-
-<slot />
+	import { setContext } from 'svelte';
+	import { client } from '$lib/graphql-client';
+  
+	// // svelte-apollo approach --> working
+	// setContext(key,
+	//   	client
+	//  )
+  
+	// not working - why?
+	import { setClient } from "svelte-apollo";
+	setClient(client);
+	
+  </script>
+  
+  <slot />
+  
